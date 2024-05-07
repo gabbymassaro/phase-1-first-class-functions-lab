d3c63d509e447be553c06bdaefbe74fa22a2db41
Gabriella Massaro <gmassaro807@gmail.com>
2024-05-07 12:46:55 -0400

    Complete lab

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = (drivers) => {
+    drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+    return drivers.slice(0, 2);
+};
+
+const returnLastTwoDrivers = (drivers) => {
+    drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+    return drivers.slice(2, 4);
+};
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(n) {
+    return function(fare) {
+        return fare * n;
+    };
+}
+
+const fareDoubler = createFareMultiplier(2);
+const fareTripler = createFareMultiplier(3); 
+
+function selectDifferentDrivers(drivers, selectingDrivers){
+    return selectingDrivers(drivers);
+}
